d41df3e1e99bb327380111a2e729795a9d565ee5
SENC <senthilc78@hotmail.com>
2020-07-15 20:06:27 +0530

    Update Class Name to AiAgent

--- DDPG/agent.py
+++ DDPG/agent.py
@@ -19,7 +19,7 @@ WEIGHT_DECAY = 0        # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class Agent():
+class AiAgent():
     """Interacts with and learns from the environment."""
     
     def __init__(self, state_size, action_size, random_seed):
